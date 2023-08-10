b006744d4d26ee6a1d0f4635131c884b99056ca8
smrm-dev <smrmdev@gmail.com>
2023-08-10 15:11:05 +0330

    feat: inherit from MuonClient

--- packages/evm/contracts/adapters/Muon/MuonAdapter.sol
+++ packages/evm/contracts/adapters/Muon/MuonAdapter.sol
@@ -1,13 +1,55 @@
 // SPDX-License-Identifier: LGPL-3.0-only
 pragma solidity ^0.8.17;
 
+import "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
 import { BlockHashOracleAdapter } from "../BlockHashOracleAdapter.sol";
+import { MuonClient } from "./utils/MuonClient.sol";
 
-contract MuonAdapter is BlockHashOracleAdapter {
+contract MuonAdapter is BlockHashOracleAdapter, MuonClient {
+
+    using ECDSA for bytes32;
+
+    struct MuonSig{
+        bytes reqId;
+        SchnorrSign sign;
+        bytes gatewaySignature;
+    }
+
+    // The apps can run their own gateway and
+    // accept the transactions that is started by the gateway.
+    address validGateway = msg.sender; // by default
 
     error ArrayLengthMissmatch(address emitter);
 
-    constructor() { }
+    constructor(
+        uint256 _muonAppId,
+        PublicKey memory _muonPublicKey
+    ) MuonClient(_muonAppId, _muonPublicKey){ }
+
+    // To get the gatewaySignature,
+    // gwSign=true should be passed to the
+    // MuonApp.
+    function verifyTSSAndGateway(
+        uint256[] memory ids,
+        bytes32[] memory _hashes,
+        MuonSig calldata muonSig
+    ) public {
+        bytes32 hash = keccak256(
+            abi.encodePacked(
+                muonAppId,
+                muonSig.reqId,
+                ids,
+                _hashes
+            )
+        );
+        bool verified = muonVerify(muonSig.reqId, uint256(hash), muonSig.sign, muonPublicKey);
+        require(verified, "TSS not verified");
+
+        hash = hash.toEthSignedMessageHash();
+        address gatewaySignatureSigner = hash.recover(muonSig.gatewaySignature);
+
+        require(gatewaySignatureSigner == validGateway, "Gateway is not valid");
+    }
 
     /// @dev Stores the hashes for a given array of idss.
     /// @param ids Array of ids number for which to set the hashes.
---
+++ packages/evm/contracts/adapters/Muon/utils/MuonClient.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import { MuonClientBase } from "./MuonClientBase.sol";
+
+contract MuonClient is MuonClientBase {
+
+    constructor(
+        uint256 _muonAppId,
+        PublicKey memory _muonPublicKey
+    ){
+        validatePubKey(_muonPublicKey.x);
+
+        muonAppId = _muonAppId;
+        muonPublicKey = _muonPublicKey;
+    }
+}
